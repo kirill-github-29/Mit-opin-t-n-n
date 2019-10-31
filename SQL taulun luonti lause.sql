50b192459960988f0107254ce05bb56a19b9a60c
PT-Jaloit <39120700+PT-Jaloit@users.noreply.github.com>
2019-10-31 12:01:41 +0200

    Update SQL taulun luonti lause.sql

--- SQL taulun luonti lause.sql
+++ SQL taulun luonti lause.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `oivallus` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `paiva` varchar(10) NOT NULL,
   `oivallus` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
